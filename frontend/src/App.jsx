feb93ad25f8a488866fc262c0936a02b1a70e284
Quang <thienquangnong@gmail.com>
2026-06-28 00:07:06 +0700

    feat: add receptionist route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import ManagerDashboardPage from './features/manager/pages/ManagerDashboardPage.jsx';
+import ReceptionistDashboardPage from './features/receptionist/pages/ReceptionistDashboardPage.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
+        <Route index element={<NotFoundPage />} />
         <Route path="manager" element={<ManagerDashboardPage />} />
+        <Route path="receptionist" element={<ReceptionistDashboardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
