0f107c9fca723495fd90b09c1b9bd5294005c365
DTN <123billbao123@gmail.com>
2026-06-27 09:47:57 +0700

    feat: remove homepage components, page, and figma prototype

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
 import ManagerDashboardPage from './features/manager/pages/ManagerDashboardPage.jsx';
-import HomePage from './pages/HomePage.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route index element={<HomePage />} />
         <Route path="manager" element={<ManagerDashboardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
