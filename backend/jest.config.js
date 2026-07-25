c78ee6e1d5949721d7e52a6976a3d3c4c81257b6
DTN <123billbao123@gmail.com>
2026-07-25 09:58:21 +0700

    fix(backend): update testMatch pattern in jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  testMatch: ['**/tests/**/*.test.js', '**/*.test.js', '**/*.spec.js'],
   clearMocks: true,
   verbose: true,
 };
+
